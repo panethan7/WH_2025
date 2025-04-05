ab425e09e5dd03363b859aa7edacd969c91d10a0
Charmaine <63215752+willowcharm6@users.noreply.github.com>
2025-04-05 16:35:01 -0500

    added background

--- 2test.py
+++ 2test.py
@@ -1,37 +1,44 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QMovie, QPixmap
 import pyautogui
 
 class TransparentWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        # Set window properties
+        # Set window properties: frameless, always on top, and transparent background
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
         
         # Get screen size to place it at bottom-left
         screen = QApplication.primaryScreen().geometry()
-        self.setGeometry(0, screen.height() - 100, 100, 100)
-
+        
         # Set the size of the window and its position
         window_width = 300  # Increase width
         window_height = 300  # Increase height
-        self.setGeometry(0, screen.height() - window_height, window_width, window_height)  # Set position and size
+        self.setGeometry(0, screen.height() - window_height, window_width, window_height)
         
-        # Create label for GIF
+        # Create background label for cat_house.png
+        self.background_label = QLabel(self)
+        self.background_label.setGeometry(0, 0, window_width, window_height)
+        pixmap = QPixmap('sprites/sheets/rooms/Room1.png')
+        self.background_label.setPixmap(pixmap)
+        self.background_label.setScaledContents(True)
+        
+        # Create label for animated GIF (cat)
         self.label = QLabel(self)
         self.label.setStyleSheet("background-color: transparent;")
-        self.label.setGeometry(0, 0, window_width, window_height)  # Resize label to match the window size
+        self.label.setGeometry(0, 0, window_width, window_height)
         
         # Load the animated GIF
         self.movie = QMovie('sprites/idle1_5cm.gif')
         self.label.setMovie(self.movie)
-        
-        # Start the animation
         self.movie.start()
+        
+        # Ensure the animated GIF label is on top of the background
+        self.label.raise_()
         
         # Show the window
         self.show()
